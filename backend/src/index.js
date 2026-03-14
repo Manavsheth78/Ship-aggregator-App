3a7139dab1b21ccc186b26eec3744c062e645e85
manavsheth78 <shethmanav2002@gmail.com>
2026-03-14 16:17:23 -0500

    update the cross-orign

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3002;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRouter);
